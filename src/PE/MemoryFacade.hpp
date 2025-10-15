6a2ac59ad890cf517b89617af978a8a8f0c81e64
Ramsensei <ramsensei@estudiantec.cr>
2025-10-15 04:15:16 -0600

    feat: memory facade to operate with main memory

---
+++ src/PE/MemoryFacade.hpp
@@ -0,0 +1,30 @@
+#ifndef MEMORY_FACADE_HPP
+#define MEMORY_FACADE_HPP
+
+#include "../components/cacheL1.h"
+#include "../interconnect/BusInterconnect.h"
+
+class MemoryFacade {
+public:
+    MemoryFacade(CacheL1* cache, BusInterconnect* bus, int pe_id)
+        : cache_(cache), bus_(bus), pe_id_(pe_id) {}
+    ~MemoryFacade() = default;
+
+    uint64_t load(uint64_t addr) {
+        uint64_t temp_;
+        cache_->read(addr, reinterpret_cast<uint8_t*>(&temp_)); // TODO: reinterpret cast must be eliminated
+        bus_->add_request(BusTransaction(pe_id_, BusCommand::BUS_READ, addr));
+        return temp_;
+    }
+    void store(uint64_t addr, uint64_t val){
+        cache_->write(addr, reinterpret_cast<const uint8_t*>(&val)); // TODO: reinterpret cast must be eliminated
+        bus_->add_request(BusTransaction(pe_id_, BusCommand::BUS_WRITE, addr));
+    }
+
+private:
+    CacheL1* cache_;
+    BusInterconnect* bus_;
+    int pe_id_; // Identificador del PE asociado
+};
+
+#endif // MEMORY_FACADE_HPP
